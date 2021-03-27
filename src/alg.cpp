8258bc593d9767430fffcfa5cf61bba9e8804f6f
romanakimkin <78901819+romanakimkin@users.noreply.github.com>
2021-03-27 15:31:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,17 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
+  int left = *arr-2;
+  int right = size; 
+  int schet = 0;
+  if (value == 0)
   return 0; // если ничего не найдено
+  else {
+  do {
+     left += 1;
+     if(value == arr[left])
+     schet += 1;
+  }while (left <= right);
+  return schet;
+  }
 }
