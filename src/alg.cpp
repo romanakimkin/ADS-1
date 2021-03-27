e54eade64dc5c3d95bf631a418574e9576c1735b
romanakimkin <78901819+romanakimkin@users.noreply.github.com>
2021-03-27 15:41:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,14 +2,14 @@
 
 int cbinsearch(int *arr, int size, int value) {
   int left = *arr-2;
-  int right = size; 
+  int right = size;
   int schet = 0;
   if (value == 0)
   return 0; // если ничего не найдено
-  else {
+  else{
   do {
      left += 1;
-     if(value == arr[left])
+     if (value == arr[left])
      schet += 1;
   }while (left <= right);
   return schet;
